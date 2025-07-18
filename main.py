cc324db965f2682a007efd3595e3808aac587b54
MOMOMALFOY <mohamedmouminchk@gmail.com>
2025-07-18 21:56:24 +0200

    requirements update

--- main.py
+++ main.py
@@ -366,4 +366,10 @@ async def accueil():
 @app.get("/ping", tags=["Healthcheck"])
 async def ping():
     """Endpoint de healthcheck pour RapidAPI."""
-    return {"status": "ok"} 
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port) 
